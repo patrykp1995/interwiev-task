3e7c9c55b8699ead230bb891768f9eb62138f5f0
patrykpilecki <patryk@codeaway.eu>
2022-01-27 11:53:23 +0100

    fix style for day weather

--- src/components/AllDayWeather.jsx
+++ src/components/AllDayWeather.jsx
@@ -7,16 +7,17 @@ const AllDayWeather = (weatherData) => {
 
   let todayConvert =  new Date(today.getTime() - today.getTimezoneOffset() * 60 * 1000).toISOString().split('T')[0]
   return (
-    <div className="allDayWeather">
+    <div >
       {Object.keys(allDayWeather).length > 0 && (
-        <div className="allDayWeather-hour">
+        <div className="allDayWeather">
           {allDayWeather.list.map((day, key) => {
             if (day.dt_txt.includes(todayConvert)) {
               return (
-                <div key={key}>
+                <div className="allDayWeather-hour" key={key}>
                    <img
                         className="weatherImg"
                         src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`}
+                        alt="weatherIcon"
                       />
                   <h1 className="weathorTemp">{day.main.temp}°C</h1>
                   <h3>{day.dt_txt.slice(10, 16)}</h3>
